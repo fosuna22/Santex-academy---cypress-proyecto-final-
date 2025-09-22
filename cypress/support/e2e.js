132d6c527986a21b68d57b5cc52430e1951c42c7
Gringodev <juanpetarcich@gmail.com>
2025-09-21 21:43:50 -0300

    Fix importación ES2015

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
-import "./commandsRegistro";
-import "./categoria";
-import "./limpiarFitro";
+import './commands'
+import './commandsRegistro'
+import './categoria'
+import './limpiarFitro'
